8ead34fa865566434cc07058a88b5c7117e39b28
MGSoni <mohitgaurav13@gmail.com>
2023-07-09 20:38:27 +0530

    Production Builder Design Pattern

---
+++ src/main/java/com/lld/designPattern/prodBuilder/Client.java
@@ -0,0 +1,21 @@
+package com.lld.designPattern.prodBuilder;
+
+public class Client {
+
+    public static void main(String[] args) {
+        /*Builder builder = new Builder();
+        builder.setAge(12);
+        builder.setName("Naman");
+        builder.setGradYear(2022);
+
+        Student student = new Student(builder);*/
+
+        Student st  = Student.getBuilder().
+                setAge(12).
+                setName("Mohit").
+                setGradYear(2022).
+                build();
+
+        System.out.println("DEBUG");
+    }
+}
---
+++ src/main/java/com/lld/designPattern/prodBuilder/Student.java
@@ -0,0 +1,116 @@
+package com.lld.designPattern.prodBuilder;
+
+public class Student {
+
+    String name;
+    int age;
+    double psp;
+    String universityName;
+    String batch;
+    long id;
+    int gradYear;
+    String phoneNumber;
+
+    public static Builder getBuilder(){
+        return new Builder();
+    }
+
+    static class Builder {
+
+        String name;
+        int age;
+        double psp;
+        String universityName;
+        String batch;
+        long id;
+        int gradYear;
+        String phoneNumber;
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getUniversityName() {
+            return universityName;
+        }
+
+        public Builder setUniversityName(String universityName) {
+            this.universityName = universityName;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Student build(){
+            if(this.gradYear>2022){
+                throw new IllegalArgumentException("Grad year cannot be greater than 2022");
+            }
+            return new Student(this);
+        }
+    }
+
+    private Student(Builder builder){
+
+        this.gradYear = builder.getGradYear();
+        this.age = builder.getAge();
+        this.name = builder.getName();
+    }
+
+}
